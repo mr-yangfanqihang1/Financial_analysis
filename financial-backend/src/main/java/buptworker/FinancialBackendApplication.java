b77b06d9dbc86efd1da1fea3663926518a587c2c
Xieyx <2022211464@bupt.cn>
2024-12-22 14:59:56 +0800

    时间模块

--- financial-backend/src/main/java/buptworker/FinancialBackendApplication.java
+++ financial-backend/src/main/java/buptworker/FinancialBackendApplication.java
@@ -2,12 +2,13 @@ package buptworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 启用定时任务
 public class FinancialBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FinancialBackendApplication.class, args);
     }
-
 }
